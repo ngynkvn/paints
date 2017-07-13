4a90154382bc6c0650aa8902b8c837057f3bd2c6
Kev <ngynkvn@gmail.com>
2017-07-13 15:35:07 -0400

    initial commit

---
+++ Arguments.java
@@ -0,0 +1,34 @@
+public class Arguments
+{
+    private final String fileOutName;
+    private final String algo;
+
+    Arguments(String [] args)
+    {
+        if(args.length == 0)
+        {
+            fileOutName = "out.png";
+            algo = "default";
+        }
+        else if(args.length == 1)
+        {
+            fileOutName = args[0];
+            algo = "default";
+        }
+        else
+        {
+            fileOutName = args[0];
+            algo = args[1];
+        }
+    }
+
+    public String getFileOutName()
+    {
+        return fileOutName;
+    }
+
+    public String getAlgo()
+    {
+        return algo;
+    }
+}
---
+++ Paints.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import javax.imageio.*;
+
+public class Paints
+{
+    public static final int IMAGE_WIDTH = 256;
+    public static final int IMAGE_HEIGHT = 128;
+
+    public static void main(String[] args)
+    {
+        Arguments a = new Arguments(args);
+
+        
+    }
+}
